0fa0e9ccf9a1aad928d31437f289c53123bb1c4e
Elton Lin <eltonlin1998@gmail.com>
2021-12-04 16:13:00 -0500

    Temporary measure for dealing with secret

--- src/lib/environmentSecrets.js
+++ src/lib/environmentSecrets.js
@@ -2,7 +2,7 @@
 import { dev } from '$app/env'
 
 if (dev) {
-  export const API_KEY_SECRET = import('./dailyCreds.js')
+  // export const API_KEY_SECRET = import('./dailyCreds.js')
 } else {
   export const API_KEY_SECRET = process.env.DAILY_API_KEY
 }

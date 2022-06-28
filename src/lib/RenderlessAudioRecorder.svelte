ebee5f563056c47f826db4ea9b336db480ac105a
Elton Lin <eltonlin1998@gmail.com>
2022-06-28 14:30:18 -0400

    Forgot to commit this file for self-adjusting timer

--- src/lib/RenderlessAudioRecorder.svelte
+++ src/lib/RenderlessAudioRecorder.svelte
@@ -1,4 +1,4 @@
-<slot {startRecording} {stopRecording} {currentTime}>
+<slot {startRecording} {stopRecording} currentTime={displayedCurrentTime}>
 
 </slot>
 
@@ -10,8 +10,63 @@
   // AudioRecorder.encoder = mpegEncoder;
   // AudioRecorder.prototype.mimeType = "audio/mpeg"; // mpeg is equivalent to mp3
   let AudioRecorder
-  let currentTime = 0
-  let timer
+
+  let startTimestamp = null // number of milliseconds since 1970 00:00:00 UTC
+  const tickSize = 100 // milliseconds
+  let etaOfNextTick = null // would use an Optional in 6.031, `0` doesn't make sense as it makes the AF inconsistent
+  let displayedCurrentTime = 0
+  let nextTimeoutID = ''
+
+  // rounds to nearest 0.1, see https://stackoverflow.com/a/12698296/7812829
+  function roundedToFixed(input, digits) {
+    var rounded = Math.pow(10, digits);
+    return (Math.round(input * rounded) / rounded).toFixed(digits);
+  }
+
+  // Timer that doesn't slowly drift late and get out of sync with visuals
+  // @see based on https://stackoverflow.com/a/29972322/7812829
+  // how the self-adjusting timer works 
+  //    - setInterval is at best, on time, but usually, late
+  //    - yes, the self-adjusting tick function will always increase time by 1 (even if 1.6 seconds has passed because it's again, late)
+  //    - ...but the 1 vs 1.6 error will be compensated next tick, because tick() will call itself after 0.4 seconds (it but it's constantly catching up) to achieve 2 vs 2
+  //    - if the tick function is super late i.e. 3 seconds passed by, it'll just call itself with setTimeout(0) 2 more times immediately
+  //    - therefore the maximum error is the infimum of the tick size. Get the tick size to be 0.1 second so the maximum error is <0.1
+  function startTimer () {
+    startTimestamp = Date.now()
+    etaOfNextTick = startTimestamp + tickSize
+    nextTimeoutID = setTimeout(
+      step, 
+      tickSize
+    )
+  }
+
+  function stopTimer () {
+    clearTimeout(nextTimeoutID)
+    nextTimeoutID = ''
+    displayedCurrentTime = 0
+    etaOfNextTick = null
+  }
+
+  function step () {
+      const delay = Date.now() - etaOfNextTick; // how late was the setTimeout 
+      if (delay > tickSize) {
+        console.log('setTimeout is lagging greatly')
+        // something really bad happened. Maybe the browser (tab) was inactive? possibly special handling to avoid futile "catch up" run
+      }
+      etaOfNextTick += tickSize
+
+      const millisecondsInSecond = 1000
+      const nearestDecimalPoint = 1
+      displayedCurrentTime = roundedToFixed(
+        (etaOfNextTick - startTimestamp) / millisecondsInSecond,
+        nearestDecimalPoint
+      )
+
+      nextTimeoutID = setTimeout(
+        step, 
+        Math.max(0, tickSize - delay)
+      )
+  }
 
   // technically this is overkill: see https://kit.svelte.dev/faq
   onMount(async () => {
@@ -56,12 +111,9 @@
       }
 
       recorder = new MediaRecorder(micStreamCopy); 
-      recorder.start();
+      recorder.start()
 
-      timer = setInterval(
-        () => currentTime += 0.1, 
-        100
-      )
+      startTimer()
 
       dispatch('record-start')
       resolve();
@@ -69,7 +121,7 @@
   }
   function stopRecording () {
     return new Promise((resolve, reject) => {
-      clearTimeout(timer)
+      stopTimer()
       recorder.addEventListener("dataavailable", e => {
         const audioBlob = e.data
         dispatch('record-end', { audioBlob }); 

3d6935231683acd8e3e00d05bd5dde3a56ea62d9
Elton Lin <eltonlin1998@gmail.com>
2022-09-05 18:27:19 -0400

    Part 1 migrating to new sveltekit

--- svelte.config.js
+++ svelte.config.js
@@ -8,23 +8,24 @@ const config = {
 		adapter: vercel(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+		// target: '#svelte',
 
 		// Fixes `require is undefined` 
 		// https://kit.svelte.dev/docs
 		// Below is Vite's config, see https://www.reddit.com/r/sveltejs/comments/nzi934/comment/h1ppot2/?utm_source=share&utm_medium=web2x&context=3
-		vite: {
-			plugins: [
-				viteCommonjs(),
-			],
+		
+		// vite: {
+		// 	plugins: [
+		// 		viteCommonjs(),
+		// 	],
 
-			// Fixes known issue with v9 Firebase and SvelteKit
-			// https://kit.svelte.dev/faq
-			// https://github.com/benmccann/sveltekit-firebase/blob/9e3097fd859e4f81e4775885ecb584561f098fd3/svelte.config.js#L11
-			ssr: {
-				external: ['whatwg-url', 'node-fetch']
-			}	
-		}
+		// 	// Fixes known issue with v9 Firebase and SvelteKit
+		// 	// https://kit.svelte.dev/faq
+		// 	// https://github.com/benmccann/sveltekit-firebase/blob/9e3097fd859e4f81e4775885ecb584561f098fd3/svelte.config.js#L11
+		// 	ssr: {
+		// 		external: ['whatwg-url', 'node-fetch']
+		// 	}	
+		// }
 	}
 };
 

877c02bc190b22d379f25aa7b502a5053ffb7bba
Elton Lin <eltonlin1998@gmail.com>
2022-12-22 21:11:33 -0500

    Work in progress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,147 @@
+<div class="flexy">
+	<div class="top-app-bar-container flexor">
+		<TopAppBar bind:this={topAppBar} variant="standard" style="background-color: hsl(0,0%,0%, 0.80);">
+			<Row>
+				<Section>
+					<img src="logo.png" width="52" height="46" style="margin-left: 0px;">
+					<Title style="font-size: 1.95rem; padding-left: 6px">
+						Explain
+					</Title>
+					<a href="https://medium.com/mit-soul" target="_blank" style="text-decoration-color: transparent;">
+						<Button style="margin-left: 40px;">
+							Blog
+						</Button>
+					</a>
+					<a href="https://github.com/verypopularguy/explain" target="_blank" style="text-decoration-color: transparent;">
+						<Button style="margin-left: 20px;">
+							Github
+						</Button>
+					</a>
+				</Section>
+
+				<Section align="end" toolbar>
+					<Button variant="outlined">
+						Log in
+					</Button>
+					<!-- 					
+					<Button on:click={() => goto('login')} variant="outlined" style="margin-left: 25px; color: rgb(128, 0, 128); font-size: 1.05rem">
+						Sign up
+					</Button>	 -->
+					<!-- <Button on:click={() => goto('login')}>Sign up</Button> -->
+				</Section>
+			</Row>
+		</TopAppBar>
+	</div>
+</div>
+
+<AutoAdjust {topAppBar}>
+	<div style="display: flex">
+		<div class="card-container" style="margin-top: 50px; margin-left: 50px;">
+			<Card>
+				<TabBar tabs={tabs} let:tab bind:active style="background-color: rgb(238, 238, 238)">
+					<!-- Note: the `tab` property is required! -->
+					<Tab {tab} minWidth indicatorSpanOnlyContent stacked>
+						<Icon class="material-icons">{tab.icon}</Icon>
+						<Label>{tab.label}</Label>
+					</Tab>
+				</TabBar>
+
+				{#if active.label === 'Students'}
+					<Content>
+						<p style="font-size: 2.5rem; font-family: sans-serif; font-weight: 500">
+							Spend less time studying, so you have space for other things in life
+						</p>
+
+						<!-- Dropdowns to select the class -->
+						<div style="width: 100%">
+							<Textfield value='' label="Which class do you want video explanations for?" style="width: 100%">
+								
+							</Textfield>
+						</div>
+
+						<!-- Then the relevant teachers -->
+						<div>
+
+						</div>
+					</Content>
+
+					<Actions>
+						<!-- Previewable -->
+
+						<!-- Add foot-note for 14.01 subsidies -->
+						<Button on:click={() => clicked++} color="secondary">
+							<Label>Sign up for $30/month</Label>
+						</Button>
+						<Button on:click={() => clicked++}>
+							<!-- <Label>Learn more</Label> -->
+						</Button>
+					</Actions>
+				{:else}
+					<Content>
+						<p style="font-size: 2.5rem; font-family: sans-serif; font-weight: 500">
+							Get paid according to impact, not time spent.
+						</p>
+
+						</Content>
+
+					<Actions>
+						<Button on:click={() => clicked++} color="secondary">
+							<Label>Schedule to interview</Label>
+						</Button>
+						<Button on:click={() => clicked++}>
+							<Label>Learn more</Label>
+						</Button>
+					</Actions>
+				{/if}
+			</Card>
+		</div>
+	</div>
+
+
+	<div>
+
+	<div style="margin-left: 50px; margin-top: 50px; font-size: 3rem; font-weight: 600; font-family: sans-serif; color: hsl(0,0%,0%, 0.80)">
+		How does it work?
+	</div>
+	<div style="margin-left: 50px; margin-right: 50px;">
+		explain.mit.edu = blackboard videos + tutoring
+	</div>
+	<div style="font-family: sans-serif; margin-left: 50px; margin-right: 50px;">
+		There's nothing that can't be understood - if explained right.
+		Private tutoring is scarce & expensive (e.g. $50/hour) because the it just doesn't scale well. 
+		
+		<br><br>
+		explain.mit.edu = video explanations + lil' bit of 1:1 help. You'd explanations for concepts you just can't understand, and problems you can't solve.
+
+		This mean you don't need to schedule, and can review the explanation, and it allows the tutor to scale themselves beyond manual labor and provide cheaper rates.
+	</div>
+	
+</div>
+
+	<div style="margin-left: 50px; margin-top: 50px; font-size: 3rem; font-weight: 600; font-family: sans-serif; color: hsl(0,0%,0%, 0.80)">
+		Why this exists
+	</div>
+
+
+
+</AutoAdjust>
+
 <section style="height: 100vh; border-bottom: 1px solid #eee;">
+	
+	
+	
 	<div style="height: 84vh; display: flex; justify-content: center; align-items: center;">
 		<div style="padding-bottom: 90px;">
-			<div style="display: flex; align-items: center; justify-content: center; height: 120px;">
-				<img src="logo.png" width="120" height="108" style="margin-left: 0px;">
-				<h1 id="logo" style="font-size: 6.5rem; color: rgb(0 0 0); padding-bottom: 16px; padding-left: 10px;">
-					explain.mit.edu
-				</h1>
-			</div>
-
 			<div style="display: flex; justify-content: center;">
-				<b style="color: grey; white-space: nowrap;" class="copied-from-koa">
-					Record and share blackboard videos quickly
-				</b>
 			</div>
 
-			<iframe 
+			<!-- <iframe 
 				src="https://giphy.com/embed/NrtarMHJrLU4sRO5mG" width="300" height="200"
 				style="position: static; margin: 0 auto; display:block; margin-top: 20px;" frameBorder="0" class="giphy-embed" allowFullScreen>
-			</iframe>
+			</iframe> -->
 
 			<!-- This flex is just used to center horizontally -->
-			<div style="display: flex; justify-content: center; margin-top: 50px">
+			<!-- <div style="display: flex; justify-content: center; margin-top: 50px">
 				<Button on:click={redirectToExampleClass} variant="outlined" color="orange" style="color: rgb(246,130,13); font-size: 1.05rem">
 					Look around
 				</Button>	
@@ -34,7 +155,7 @@
 						Resume
 					</Button>	
 				{/if}
-			</div>
+			</div> -->
 	</div>
 </section>
 
@@ -44,6 +165,25 @@
 	import { onMount, tick, onDestroy } from 'svelte'
 	import { goto } from '$app/navigation'
 	import { user } from '../store.js'
+	import TabBar from '@smui/tab-bar';
+	import Tab, { Icon } from '@smui/tab';
+	import TopAppBar, { Row, Section, Title, AutoAdjust } from '@smui/top-app-bar';
+	import IconButton from '@smui/icon-button';
+	import Card, {
+    Content,
+    PrimaryAction,
+    Actions,
+    ActionButtons,
+    ActionIcons,
+  } from '@smui/card';
+	import Textfield from '@smui/textfield'
+
+	let topAppBar
+
+	let tabs = [{ label: 'Students', icon: 'hail'}, { label: 'Teachers', icon: 'local_taxi'}]
+	let active = tabs[0]
+
+	let foo
 
 	function resumeToMostRecentServer () {
 		goto($user.mostRecentClassAndRoomID, { replaceState: true })
@@ -95,6 +235,34 @@ li {
 #make-your-own-video {
 
 }
+
+:global(#smui-app),
+  :global(body),
+  :global(html) {
+    display: block !important;
+    height: auto !important;
+    width: auto !important;
+    position: static !important;
+  }
+
+
+	.flexy {
+    display: flex;
+    flex-wrap: wrap;
+  }
+ 
+  .flexor {
+    display: inline-flex;
+    flex-direction: column;
+  }
+
+	.card-container {
+		width: 600px;
+	}
+
+	.mdc-tab__text-label {
+		color: orange;
+	}
 
 </style>
 

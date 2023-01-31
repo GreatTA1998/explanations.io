a9fe95b39640e58b54611e8b1c3741065f673c7c
Elton Lin <eltonlin1998@gmail.com>
2023-01-31 11:20:14 -0500

    Try to use only number pin pads

--- src/lib/PhoneLogin.svelte
+++ src/lib/PhoneLogin.svelte
@@ -4,13 +4,13 @@
       <!-- <div class="copied-from-koa" style="color: purple">Log in with phone:</div> -->
 
       {#if canTakeInternationalNumbers}
-        <input type="tel" id="phone-country-code" minlength="2" maxlength="4" placeholder="+1" bind:value={countryCode} class="phoneNumberFourDigitInput" style="margin-left: 15px; margin-right: 10px">
+        <input type="tel" inputmode="numeric" pattern="[0-9]*" id="phone-country-code" minlength="2" maxlength="4" placeholder="+1" bind:value={countryCode} class="phoneNumberFourDigitInput" style="margin-left: 15px; margin-right: 10px">
       {/if}
-      <input type="tel" id="phone-input-1" minlength="3" maxlength="3" placeholder="503" bind:value={phoneNumSegment1} class="phoneNumberThreeDigitInput" style="margin-left: 15px; margin-right: 10px">
+      <input type="tel" inputmode="numeric" pattern="[0-9]*" id="phone-input-1" minlength="3" maxlength="3" placeholder="503" bind:value={phoneNumSegment1} class="phoneNumberThreeDigitInput" style="margin-left: 15px; margin-right: 10px">
 
-      <input type="tel" id="phone-input-2" minlength="3" maxlength="3" placeholder="250" bind:value={phoneNumSegment2} class="phoneNumberThreeDigitInput" style="margin-right: 10px">
+      <input type="tel" inputmode="numeric" pattern="[0-9]*" id="phone-input-2" minlength="3" maxlength="3" placeholder="250" bind:value={phoneNumSegment2} class="phoneNumberThreeDigitInput" style="margin-right: 10px">
 
-      <input type="tel" id="phone-input-3" minlength="4" maxlength="4" placeholder="3868" bind:value={phoneNumSegment3} class="phoneNumberFourDigitInput" style="margin-right: 10px">
+      <input type="tel" inputmode="numeric" pattern="[0-9]*" id="phone-input-3" minlength="4" maxlength="4" placeholder="3868" bind:value={phoneNumSegment3} class="phoneNumberFourDigitInput" style="margin-right: 10px">
       <Button id="sign-in-button" on:click={signInWithPhone} style="display: none; color: {hasEnteredPhoneNumber ? 'rgb(116 28 183)' : 'grey'}; margin-bottom: 2px" disabled={!hasEnteredPhoneNumber}>
         Sign Up
       </Button>
@@ -22,12 +22,12 @@
 
     <div style="display: flex; justify-content: center; align-items: center; margin-top: 24px">      
       <!-- `numeric`: https://stackoverflow.com/a/62179845/7812829 -->
-      <input type="text" inputmode="numeric" minlength="1" maxlength="1" bind:value={confirm1} bind:this={c1} class="confirmCodeSingleDigitInput" placeholder="1">
-      <input type="text" inputmode="numeric" minlength="1" maxlength="1" bind:value={confirm2} bind:this={c2} class="confirmCodeSingleDigitInput" placeholder="2">  
-      <input type="text" inputmode="numeric" minlength="1" maxlength="1" bind:value={confirm3} bind:this={c3} class="confirmCodeSingleDigitInput" placeholder="3">
-      <input type="text" inputmode="numeric" minlength="1" maxlength="1" bind:value={confirm4} bind:this={c4} class="confirmCodeSingleDigitInput" placeholder="4">
-      <input type="text" inputmode="numeric" minlength="1" maxlength="1" bind:value={confirm5} bind:this={c5} class="confirmCodeSingleDigitInput" placeholder="5">
-      <input type="text" inputmode="numeric" minlength="1" maxlength="1" bind:value={confirm6} bind:this={c6} class="confirmCodeSingleDigitInput" placeholder="6">
+      <input type="text" inputmode="numeric" pattern="[0-9]*" minlength="1" maxlength="1" bind:value={confirm1} bind:this={c1} class="confirmCodeSingleDigitInput" placeholder="1">
+      <input type="text" inputmode="numeric" pattern="[0-9]*" minlength="1" maxlength="1" bind:value={confirm2} bind:this={c2} class="confirmCodeSingleDigitInput" placeholder="2">  
+      <input type="text" inputmode="numeric" pattern="[0-9]*" minlength="1" maxlength="1" bind:value={confirm3} bind:this={c3} class="confirmCodeSingleDigitInput" placeholder="3">
+      <input type="text" inputmode="numeric" pattern="[0-9]*" minlength="1" maxlength="1" bind:value={confirm4} bind:this={c4} class="confirmCodeSingleDigitInput" placeholder="4">
+      <input type="text" inputmode="numeric" pattern="[0-9]*" minlength="1" maxlength="1" bind:value={confirm5} bind:this={c5} class="confirmCodeSingleDigitInput" placeholder="5">
+      <input type="text" inputmode="numeric" pattern="[0-9]*" minlength="1" maxlength="1" bind:value={confirm6} bind:this={c6} class="confirmCodeSingleDigitInput" placeholder="6">
       <Button on:click={verifyConfirmationCode} style="display: none; color: {hasEnteredConfirmCode ? 'rgb(116 28 183)' : 'grey'}; margin-bottom: 2px;" disabled={!hasEnteredConfirmCode}>Confirm code</Button>
     </div>
   {/if}

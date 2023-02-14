b38bd969f0c0fc1a336b0fee886fbfe3d068d26d
Elton Lin <eltonlin1998@gmail.com>
2023-02-14 11:25:25 -0500

    Add banner warning and waitlist link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,6 +32,19 @@
 </div>
 
 <AutoAdjust {topAppBar}>
+	<div class="top-banner-to-warn-experimental">
+		This website is still in beta, with a small number of users, known bugs and constant updates.
+		<a 
+			href="https://docs.google.com/forms/d/e/1FAIpQLSdEMySRDoAi3mBSF2G6hsbDhf--l1ayqPd1p0AvdOtJOvrzIw/viewform?usp=sf_link" 
+			target="_blank"
+			style="color: white;"
+		>
+			Join waitlist here
+		</a>
+		<div style="margin-right: 0; margin-left: auto;">
+			Tech support: 503 250 3868 / eltonlin@mit.edu
+		</div>
+	</div>
 	<TabBar tabs={tabs} let:tab bind:active style="background-color: rgb(238, 238, 238)" variant="dense">
 		<Tab {tab} stacked minWidth indicatorSpanOnlyContent>
 			<Icon class="material-icons">{tab.icon}</Icon>
@@ -353,6 +366,7 @@
 	import TabBar from '@smui/tab-bar';
 	import Tab, { Icon } from '@smui/tab';
 	import TopAppBar, { Row, Section, Title, AutoAdjust } from '@smui/top-app-bar';
+	import Banner from '@smui/banner';
 	import Card, {
     Content,
     PrimaryAction,
@@ -385,6 +399,16 @@
 </script>
 
 <style lang="scss">
+	.top-banner-to-warn-experimental {
+		background-color: green;
+		font-family: sans-serif;
+		padding-left: 16px;
+		padding-right: 10px;
+		padding-top: 4px;
+		padding-bottom: 4px;
+		color: white;
+	}
+
 	.webflow-h1 {
 		font-size: 4rem;
 	}

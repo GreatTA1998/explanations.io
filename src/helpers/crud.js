969a889216661550d8d9682e593f7d63f38d98e3
Elton Lin <eltonlin1998@gmail.com>
2023-04-21 19:06:09 -0400

    Improve website based on Jonathan's feedback

--- src/helpers/crud.js
+++ src/helpers/crud.js
@@ -23,7 +23,7 @@ export function getFirestoreDoc (path) {
     const ref = firestoreRef(path)
     const snapshot = await getDoc(ref)
     resolve(
-      { id: snapshot.id, path: snapshot.path, ...snapshot.data() }
+      { id: snapshot.id, path: snapshot.ref.path, ...snapshot.data() }
     )
   })
 }
@@ -34,7 +34,7 @@ export function getFirestoreCollection (path) {
     const snapshot = await getDocs(ref)
     const data = []
     snapshot.forEach(doc => {
-      data.push({ id: doc.id, path: doc.path, ...doc.data() })
+      data.push({ id: doc.id, path: doc.ref.path, ...doc.data() })
     })
     resolve(data)
   })
@@ -127,3 +127,46 @@ export async function createBoardDoc (boardsDbPath, roomRef) {
     resolve(newID)
   })
 }
+
+// BACKWARDS COMPATIBILITY SCRIPT FUNCTIONS
+export async function changeVideoCreatorInfo () {
+  const boards = await getFirestoreCollection('classes/Mev5x66mSMEvNz3rijym/blackboards')
+  for (const board of boards) {
+    let n = 3
+    if (board.creatorUID === 'Q0J071uuDfdJVxTubCkYVhsTAxR2') {
+      console.log('found Jonathan Whyte\'s board')
+      n += 3
+      const creatorDoc = await getFirestoreDoc('users/' + board.creatorUID)
+      await updateFirestoreDoc(`classes/Mev5x66mSMEvNz3rijym/blackboards/${board.id}`, {
+        creatorName: 'Jonathan Whyte',
+        shopGalleryOrder: n
+      })
+    }
+  }
+}
+
+// this could be useful in the future, but it's only a temporary script now
+export async function moveVideoToDifferentClassServer (videoPath) {
+  console.log("videoPath =", videoPath)
+  const boardDoc = await getFirestoreDoc(videoPath)
+  const strokesDocs = await getFirestoreCollection(videoPath + '/strokes')
+
+  // create an independent copy of the video in the new class
+  const newBoardPath = `classes/Mev5x66mSMEvNz3rijym/blackboards/${boardDoc.id}`
+  const newStrokesPath = newBoardPath + '/strokes/'
+
+  setFirestoreDoc(newBoardPath, boardDoc)
+  for (const stroke of strokesDocs) {
+    setFirestoreDoc(newStrokesPath + stroke.id, stroke)
+  }
+  console.log('initiated copying of drawing strokes')
+
+
+  // you also need to create a room doc
+  const newRoomID = await createRoomDoc('classes/Mev5x66mSMEvNz3rijym/')
+  console.log('newRoomID =', newRoomID)
+  updateFirestoreDoc('classes/Mev5x66mSMEvNz3rijym/rooms/' + newRoomID, {
+    blackboards: arrayUnion(boardDoc.id)
+  })
+}
+

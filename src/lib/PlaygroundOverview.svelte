4dbd570efafe098b1480d1a6fa3badf66e840763
Elton Lin <eltonlin1998@gmail.com>
2023-10-02 18:17:25 -0400

    Work in progress

--- src/lib/PlaygroundOverview.svelte
+++ src/lib/PlaygroundOverview.svelte
@@ -1,4 +1,4 @@
-<div style="display: flex; justify-content: center">
+<!-- <div style="display: flex; justify-content: center">
   <input 
     class="" 
     name="search" 
@@ -16,43 +16,86 @@
       padding: 12px;
     "
   >
-</div>
+</div> -->
 
-<div style="display: flex; justify-content: space-around; margin-top: 20px; width: 50%; padding-left: 80px;">
-  {#each categories as category}
-    <div style="border: 1px solid grey; border-radius: 4px; height: 40px; font-size: 1.4em; padding: 8px; padding-top: 16px;">
+
+<div style="display: flex; margin-top: 20px; width: fit-content; padding-left: 60px; flex-wrap: wrap;">
+  {#each categories as category, i}
+    <div 
+      style="
+        border: 1px solid grey; 
+        margin-left: 10px; 
+        border-radius: 4px; 
+        height: 50px;
+        padding: 16px;
+        white-space: nowrap;
+        font-size: 2.5em; 
+        margin-top: 1%;
+      "
+      class:orange-highlight={i === 0}
+    >
       {category}
     </div>
   {/each}
 </div>
 
-<div style="margin-left: 88px; margin-top: 20px;">
+<div style="padding-left: 80px; display: flex; margin-top: 20px; justify-content: space-around; width: fit-content; align-items: center;">
+  <div style="font-size: 1.3em">
+    Filters:
+  </div>
+  <div class="filter-tag" style="background-color: white; color: black;">
+    All servers
+  </div>
+  <div class="filter-tag" style="background-color: white; color: green;">
+    Has teachers & students
+  </div>
+  <div class="filter-tag" style="background-color: white; color: red;">
+    Need teachers
+  </div>
+  <div class="filter-tag" style="background-color: white; color: red;">
+    Need students
+  </div>
+  <!-- <div class="filter-tag">
+    Need subscribers
+  </div> -->
+</div>
+
+<div style="margin-top: 40px; display: flex; flex-wrap: wrap;">
   {#each serversInCategory as server}
     <div 
       style="
         border: 2px solid grey; 
         border-radius: 8px;
-        width: 600px;
-        padding: 30px; 
+        width: 550px;
         margin-bottom: 20px;
+        padding-top: 30px;
+        margin-left: 4%;
       "
     >
-      <div style="font-size: 2em;">
+      <div style="font-size: 2em; margin-left: 20px;">
         {server.name}
+        {#if server.description}
+         ({server.description})
+        {/if}
       </div>
-      <!-- 
-        <div>
-          {server.description}
-        </div> 
-      -->
-
-      <div style="display: flex">
-        {server.collegeSpecificType} | n members
+      <div style="display: flex; margin-left: 21px; opacity: 70%; font-size: 1.3em; align-items: center; font-weight: 500;">
+        {server.collegeSpecificType} 
+        <span class="material-icons" style="font-size: 4px; margin: 8px;">circle</span>
+        n unique visitors
       </div>
 
       <div style="margin-top: 8px;"></div>
 
-      {#if server.numOfUnresolvedQuestions + server.numOfResolvedQuestions === 0} 
+      <TwosidedMarketplace
+        numOfStudents={server.numOfStudents || 0}
+        numOfTeachers={server.numOfCreators}
+        numOfVideos={server.numOfVideos}
+        numOfQuestions={server.numOfUnresolvedQuestions + server.numOfResolvedQuestions}
+      >
+
+      </TwosidedMarketplace>
+
+      <!-- {#if server.numOfUnresolvedQuestions + server.numOfResolvedQuestions === 0} 
         <div style="color: red; display: flex; align-items: center;">
           <span class="material-icons">
             priority_high
@@ -67,11 +110,9 @@
           <div style="display: flex;">
             {server.numOfUnresolvedQuestions + server.numOfResolvedQuestions} 
             questions asked,
-            <!-- {#if server.numOfUnresolvedQuestions > 0} -->
-              <div style="color: {server.numOfUnresolvedQuestions > 0 ? 'red' : ''}; margin-left: 4px;">
-                {server.numOfUnresolvedQuestions} unresolved questions
-              </div>
-            <!-- {/if}  -->
+            <div style="color: {server.numOfUnresolvedQuestions > 0 ? 'red' : ''}; margin-left: 4px;">
+              {server.numOfUnresolvedQuestions} unresolved questions
+            </div>
           </div>
         </div>
       {/if}
@@ -80,23 +121,26 @@
           priority_high
         </span>
         Need explanation creators
-      </div>
+      </div> -->
 
       <div style="margin-top: 14px;">
 
       </div>
-
-      <ReusableButton color="info">
-        Join server
-      </ReusableButton>
+      <div style="width: 180px; margin-bottom: 24px; margin-left: 24px;">
+        <ReusableButton color="primary">
+          Go to server
+        </ReusableButton>
+      </div>
+ 
     </div>
   {/each}
 </div>
 
 <script>
   import ReusableButton from "$lib/ReusableButton.svelte";
+  import TwosidedMarketplace from "$lib/TwosidedMarketplace.svelte";
 
-  let categories = ['All subjects', 'Math', 'Mechanical Engineering', 'Nursing', 'Chemistry', 'Computer Science', ]
+  let categories = ['All subjects', 'Math', 'Mechanical Engineering', 'Nursing', 'Chemistry', 'Computer Science', 'Others' ]
 
   let serversInCategory = [
     { 
@@ -106,11 +150,21 @@
       numOfVideos: 0,
       numOfUnresolvedQuestions: 0,
       numOfResolvedQuestions: 0,
-      collegeSpecificType: 'All colleges'
+      collegeSpecificType: 'Roxbury Community College'
+    },
+    { 
+      name: 'Linear Algebra',
+      // description: 'Roxbury Community College',
+      numOfCreators: 2,
+      numOfVideos: 36,
+      numOfUnresolvedQuestions: 0,
+      numOfResolvedQuestions: 0,
+      collegeSpecificType: 'Roxbury Community College'
     },
     {
-      name: '2.001: Mechanics of Materials',
-      // description: 'MIT Mechanics of Materials',
+      name: '2.001',
+      description: 'Mechanics of Materials',
+      numOfStudents: 2,
       numOfCreators: 1,
       numOfVideos: 3,
       numOfUnresolvedQuestions: 0,
@@ -120,11 +174,31 @@
     {
       name: 'Accounting',
       description: '',
+      numOfStudents: 1,
       numOfCreators: 0,
       numOfVideos: 0,
       numOfUnresolvedQuestions: 1,
       numOfResolvedQuestions: 0,
-      collegeSpecificType: 'Massachusetts Institute of Technology'
+      collegeSpecificType: 'Non-college specific'
     }
   ]
 </script>
+
+<style>
+  .filter-tag {
+    background-color: red; 
+    color: white; 
+    padding: 12px; 
+    border-radius: 24px;
+    margin-left: 12px;
+    border: 1px solid black;
+    font-weight: 600;
+    font-size: 1.8em;
+  }
+
+  .orange-highlight {
+    background-color: hsl(0,0%,0%, 0.80);
+    color: white;
+    border: 1px solid white;
+  }
+</style>
---
+++ src/lib/TwosidedMarketplace.svelte
@@ -0,0 +1,91 @@
+<div style="display: flex;">
+  <div class="one-side-of-market" style="border-right: 1px solid black">
+    <div 
+      class:red-text={numOfStudents === 0}
+      style="font-size: 1em; 
+      
+      font-weight: 500; display: flex; justify-content: center; align-items: center;"
+    >
+      {numOfStudents} Student{numOfStudents === 1 ? '' : 's'}
+      <span class="material-icons" style="font-size: 4px; margin: 8px;">circle</span>
+      {numOfQuestions} questions asked
+    </div>
+    
+    <div style="display: flex; margin-top: 12px; padding-left: 16px;">
+      {#each {length: numOfStudents} as _, i}
+        <span class="material-icons" style="font-size: 3em; color: black;">
+          emoji_people
+        </span>
+      {/each}
+   </div>
+
+   <!-- <div class:red-text={numOfQuestions === 0} 
+      style="margin-top: 36px; display: flex; justify-content: center;"
+    >
+      {numOfQuestions} questions asked
+    </div> -->
+
+   <div style="display: flex; align-items: center; padding-left: 20px; margin-top: 12px;">
+      {#each {length: numOfQuestions} as _, i}
+        <span class="material-icons" style="font-size: {2 * (0.97 ** numOfVideos)}em; color: black;">
+          question_mark
+        </span>
+      {/each}
+    </div>
+
+  </div>
+  <div class="one-side-of-market" style="border-left: 1px black;">
+    <div 
+      class:red-text={numOfTeachers === 0}
+      style="
+        font-size: 1em; 
+        font-weight: 500;
+        display: flex; 
+        justify-content: center;    
+      "
+    >
+      {numOfTeachers} Teacher{numOfTeachers === 1 ? '' : 's'}
+      <span class="material-icons" style="font-size: 4px; margin: 8px;">
+        circle
+      </span>
+      {numOfVideos} videos created
+    </div>
+
+    <div style="display: flex; margin-top: 12px; padding-left: 24px;">
+      {#each {length: numOfTeachers} as _, i}
+        <span class="material-icons" style="font-size: 3em;">
+          school
+        </span>
+      {/each}
+   </div>
+   <div style="padding-left: 24px; display: flex; align-items: center; margin-top: 12px; flex-wrap: wrap;">
+      {#each {length: numOfVideos} as _, i}
+        <span class="material-icons" style="font-size: {2 * (0.97 ** numOfVideos)}em;">
+          slideshow
+        </span>
+      {/each}
+    </div>
+  </div>
+</div>
+
+<script>
+  export let numOfStudents
+  export let numOfTeachers
+  export let numOfVideos
+  export let numOfQuestions
+</script>
+
+<style>
+  .one-side-of-market {
+    width: 50%; 
+    height: fit-content; 
+    padding-top: 20px;
+    padding-bottom: 10px;
+    /* border: 1px solid purple; */
+    /* border-radius: 22px; */
+  }
+
+  .red-text {
+    color: red;
+  }
+</style>

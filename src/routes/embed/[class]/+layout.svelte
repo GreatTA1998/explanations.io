7755a9515c155d2b811d14cd7ce1afcd933a4d17
Elton Lin <eltonlin1998@gmail.com>
2024-11-22 13:21:48 +0900

    Fix top-app-bar error by using 6.2.0 for SMUI

--- src/routes/embed/[class]/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<slot>
-
-</slot>
-
-<script>
-  export let data;
-   let { classID, blackboardID } = data;
-   $: ({ classID, blackboardID } = data); // so it stays in sync when `data` changes
-</script>
